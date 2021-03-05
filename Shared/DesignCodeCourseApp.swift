4ce0c0d99fba4aa4afaba8a374cd25fd43d6c3e4
grav0211 <wbiosd111@gmail.com>
2021-03-05 18:09:44 -0500

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by Alexandre Gravelle on 2021-03-05.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
