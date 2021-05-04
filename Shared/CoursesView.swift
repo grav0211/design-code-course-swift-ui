5795ccd9d77af98f5748ec645b358def0318ef96
grav0211 <wbiosd111@gmail.com>
2021-05-04 15:32:14 -0400

    Add Course model. Update CoursesView to accept model data.

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -15,21 +15,21 @@ struct CoursesView: View {
         ZStack {
             ScrollView {
                 VStack(spacing: 20) {
-                    CourseItem()
-                        .matchedGeometryEffect(id: "Card", in: namespace, isSource: !show)
-                        .frame(width: 335, height: 250)
-                    CourseItem()
-                        .frame(width: 335, height: 250)
+                    ForEach(courses) { item in
+                        CourseItem(course: item)
+                            .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+                            .frame(width: 335, height: 250)
+                    }
                 }
                 .frame(maxWidth: .infinity)
             }
             
             if show {
                 ScrollView {
-                    CourseItem()
-                        .matchedGeometryEffect(id: "Card", in: namespace)
+                    CourseItem(course: courses[2])
+                        .matchedGeometryEffect(id: courses[0].id, in: namespace)
                         .frame(height: 300)
-                    VStack(spacing: 8.0) {
+                    VStack {
                         ForEach(0 ..< 20) { item in
                             CourseRow()
                         }
